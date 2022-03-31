a3eb8027e5488bb7f6c37d499dadf12ced440b92
ijuso <72605682+ijuso@users.noreply.github.com>
2022-03-31 18:06:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // imports
 var express = require('express')
 var app = express()
-var fs = require('fs');
-var bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
-
+var fs = require('fs')
+var bodyParser = require("body-parser")
+const PORT = process.env.PORT || 5000
 
 //staattiset filet
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/css'))
-app.use('/img', express.static(__dirname + 'public/css'))
-
 //hakee etusivun
 app.get('', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -19,67 +14,50 @@ app.get('', (req, res) => {
 //hakee vieraskirja datan
 app.get('/guestbook', (req, res) => {
     
-    var listdata = require(__dirname + '/public/data/lista.json');
+  var listdata = require(__dirname + '/public/data/lista.json');
      
-    var results = '<table border="1"> ';
-      for (var i = 0; i < listdata.length; i++) {
-      results +=
-         '<tr>' +
-         '<td>' + listdata[i].username + '</td>' +
-         '<td>' + listdata[i].country + '</td>' +
-         '<td>' + listdata[i].message + '</td>' +
-         '</tr>';
+  //tulostaa taulukon sivulle vieraista
+  var results = '<table border="1"> ';
+    for (var i = 0; i < listdata.length; i++) {
+    results +=
+       '<tr>' +
+       '<td>' + listdata[i].username + '</td>' +
+       '<td>' + listdata[i].country + '</td>' +
+       '<td>' + listdata[i].message + '</td>' +
+       '</tr>';
    } 
    res.send(results);
 });
 
-
-
-
-//newmessgae
+//näyttää uusikirjaus formin
 app.get('/newmessage', (req, res) => {
     res.sendFile(__dirname + '/views/newmessage.html')
 })
 
-
-
-
-//post juttu
+//post juttu, tallentaa formiin laitetut jutut
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/kirjaudu", function (req, res) {
-    console.log("ebin");
     
+  var listdata = require(__dirname + '/public/data/lista.json');
+  listdata.push({
+    id: listdata.length + 1,
+      username: req.body.username,
+      country: req.body.country,
+      message: req.body.message,
+      date: new Date()
+  });
+  //muokataan json objekti stringi muotoon
+  var jsonStr = JSON.stringify(listdata);
 
-    var listdata = require(__dirname + '/public/data/lista.json');
-    listdata.push({
-        id: listdata.length + 1,
-        username: req.body.username,
-        country: req.body.country,
-        message: req.body.message,
-        date: new Date()
-    });
-    
-       // Convert the JSON object to a string format
-    var jsonStr = JSON.stringify(listdata);
-
-  // Write data to a file
+  //kirjoitetaan data tiedostoon
   fs.writeFile("./public/data/lista.json", jsonStr, err => {
     if (err) throw err;
-    console.log("It's saved!");
+    console.log("tallennettu");
   });
   res.send(
-    "Saved the data to a file."
-  );
-
-    res.send("vittu jee" + username + country + message); 
+    "tallennettu."
+  ); 
 });
 
-
-
-
-
-
-
-
-//listen on port 3000
-app.listen(PORT, () => console.info('Listening on port 3000'))
+//kuunnellaan
+app.listen(port, () => console.info('Listening on port ' + port))
